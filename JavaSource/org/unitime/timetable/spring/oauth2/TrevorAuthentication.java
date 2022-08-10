72611f9a395e10c2b7cbcc3fc8f4cd617e1534fb
Trevor Claridge <trevclaridge@gmail.com>
2022-08-10 01:41:08 -0400

    WIP: Jwt validation

--- JavaSource/org/unitime/timetable/spring/oauth2/TrevorAuthentication.java
+++ JavaSource/org/unitime/timetable/spring/oauth2/TrevorAuthentication.java
@@ -37,11 +37,43 @@ import com.google.gwt.user.client.Window;
 import org.springframework.security.ldap.authentication.LdapAuthenticator;
 import org.springframework.security.authentication.TestingAuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationProvider;
+// import java.net.http.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Base64.Decoder;
+import java.util.Base64;
+// import org.omg.DynamicAny.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+// import java.net.http.HttpResponse;
+import java.io.InputStream;
+import java.lang.Exception;
+// import org.apache.hc.client5.http.classic.methods.HttpPost;
+// import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
+// import org.apache.hc.core5.http.HttpEntity;
+// import org.apache.hc.core5.http.io.entity.EntityUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
-
+// import javax.json.JSONObject;
+// import javax.json.JSONString;
+// import org.json.simple.JSONObject;
+// import jakarta.json.JsonObject;
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonElement;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.SecretKeySpec;
+import io.jsonwebtoken.DefaultJwtSignatureValidator;
 
 public class TrevorAuthentication implements AuthenticationProvider {
 	private static Log sLog = LogFactory.getLog(TrevorAuthentication.class);
+
 
 	public TrevorAuthentication() {
 		sLog.info("TREVOR CLARIDGE: TrevorAuthentication.");
@@ -55,8 +87,69 @@ public class TrevorAuthentication implements AuthenticationProvider {
         String authCode = authentication.getPrincipal().toString();
         sLog.info("test password : " + testPassword);
         sLog.info("authcode: " + authCode);
-        authentication.setAuthenticated(true);
 
+        // https://mkyong.com/java/how-to-send-http-request-getpost-in-java/
+        CloseableHttpClient httpClientMaster = HttpClients.createDefault();
+        HttpPost post = new HttpPost("https://login.microsoftonline.com/d958f048-e431-4277-9c8d-ebfb75e7aa64/oauth2/v2.0/token");
+
+        // add request parameter, form parameters
+        List<NameValuePair> urlParameters = new ArrayList<>();
+        urlParameters.add(new BasicNameValuePair("client_id", "98ae7ee1-eb75-4a49-a7c0-c7074eb64e02"));
+        urlParameters.add(new BasicNameValuePair("scope", "openid"));
+        urlParameters.add(new BasicNameValuePair("code", authCode));
+        urlParameters.add(new BasicNameValuePair("redirect_uri", "https://unitime-ssotest.wallawalla.edu/UniTime/selectPrimaryRole.do"));
+        urlParameters.add(new BasicNameValuePair("grant_type", "authorization_code"));
+
+        try {
+            post.setEntity(new UrlEncodedFormEntity(urlParameters));
+        } catch(Exception e) {
+            sLog.info(e.toString());
+        }
+
+        try (CloseableHttpClient httpClient = HttpClients.createDefault();
+             CloseableHttpResponse response = httpClient.execute(post)) {
+            
+            Gson gson = new Gson();
+            JsonObject body = gson.fromJson(EntityUtils.toString(response.getEntity()), JsonObject.class);
+            String token = body.get("error").getAsString();
+            sLog.info("TOKEN: " +  token);
+
+            String testToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkJhZWxkdW5nIFVzZXIiLCJpYXQiOjE1MTYyMzkwMjJ9.qH7Zj_m3kY69kxhaQXTa-ivIpytKXXjZc1ZSmapZnGE";
+
+            String[] chunks = testToken.split("\\.");
+            Base64.Decoder decoder = Base64.getUrlDecoder();
+
+            String header = new String(decoder.decode(chunks[0]));
+            String payload = new String(decoder.decode(chunks[1]));
+            String signature = new String(decoder.decode(chunks[2]));
+
+            sLog.info("Header: " + header);
+            sLog.info("Payload: " + payload);
+            sLog.info("Signature: " + signature);
+
+            String tokenWithoutSignature = header + "." + payload;
+            
+            SignatureAlgorithm sa = HS256;
+            SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), sa.getJcaName());
+            DefaultJwtSignatureValidator validator = new DefaultJwtSignatureValidator(sa, secretKeySpec);
+
+            if (!validator.isValid(tokenWithoutSignature, signature)) {
+                sLog.info("signature not valid");
+                throw new Exception("Could not verify JWT token integrity!");
+            }
+
+        } catch(Exception e) {
+            sLog.info(e.toString());
+        }  
+
+        try {
+            httpClientMaster.close();
+        } catch(Exception e) {
+            sLog.info(e.toString());
+        }
+
+
+        authentication.setAuthenticated(true);
         return authentication;
     }
 
